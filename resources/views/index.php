4195f03b787072c5ec4dd645bd372acc94cc8aee
fgt31337 <sidan1994@gmail.com>
2020-01-03 16:39:50 +0900

    Bootstrap fix

--- resources/views/index.php
+++ resources/views/index.php
@@ -6,25 +6,20 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="../vendor/twbs/bootstrap/dist/css/bootstrap.min.css" >
-
-        <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">-->
+        <link rel="stylesheet" href="twbs/bootstrap/dist/css/bootstrap.min.css" type="text/css" >
 
         <title>Hello, world!</title>
     </head>
     <body>
 
         <h1 align="center">Hello, world!</h1>
-
+        <div class="alert alert-primary" role="alert">
+  A simple primary alert—check it out!
+</div>
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="../vendor/components/jquery/jquery.min.js"></script>
-        <script src="../vendor/twbs/bootstrap/dist/js/popper.min.js"></script>
-        <script src="../vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
-
-        <!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script> -->
-
+        <script src="components/jquery/jquery.min.js"></script>
+        <script src="components/popper/node_modules/popper.js/dist/umd/popper.min.js"></script>
+        <script src="twbs/bootstrap/dist/js/bootstrap.min.js"></script>
     </body>
 </html>
